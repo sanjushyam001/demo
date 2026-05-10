8b94ca399938cfa376e62bfb20c58ade3ef3a483
Sanjeev Kumar <kumarsanjuashokit@gmail.com>
2026-05-11 01:16:51 +0530

    Changes are done in DemoMainClass in develop branch

--- src/main/java/com/eazybytes/demo/DemoMainClass.java
+++ src/main/java/com/eazybytes/demo/DemoMainClass.java
@@ -2,6 +2,7 @@ package com.eazybytes.demo;
 
 public class DemoMainClass {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World1!");
+		System.out.println("Hello World2!");
     }
 }
